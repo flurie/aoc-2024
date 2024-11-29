1824c6e80f6290783d06e29a0080a8a1427510bf
flurie <flurie@gmail.com>
2024-11-29 11:28:40 -0500

    configure cpp library through bazel

---
+++ README.md
@@ -0,0 +1,23 @@
+# AOC 2024
+
+A dummy repo set up to do Advent of Code in C++.
+
+## Setup
+
+This should work equally well in either macOS or Linux.
+
+The following prerequisites must be installed:
+
+- Nix
+- devenv
+- direnv (optional)
+
+Everything else you need is installed using those.
+
+## Architecture
+
+| Tool  | choice  |
+|-------|---------|
+| LSP   | clangd  |
+| build | Bazel 7 |
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,51 @@
-#include <iomanip>
 #include <iostream>
-#include <ranges>
-#include <string_view>
 
-int main() {
-  using std::operator""sv;
-  constexpr auto words{"Hello^_^C++^_^20^_^!"sv};
-  constexpr auto delim{"^_^"sv};
+#include <reproc++/run.hpp>
 
-  for (const auto word : std::views::split(words, delim))
-    // with string_view's C++23 range constructor:
-    std::cout << std::quoted(std::string_view(word)) << ' ';
-  std::cout << '\n';
+// Equivalent to reproc's run example but implemented using reproc++.
+int main(int argc, const char **argv) {
+  (void)argc;
+
+  int status = -1;
+  std::error_code ec;
+
+  reproc::options options;
+  options.redirect.parent = true;
+  options.deadline = reproc::milliseconds(5000);
+
+  std::tie(status, ec) = reproc::run(argv + 1, options);
+
+  if (ec) {
+    std::cerr << ec.message() << std::endl;
+  }
+
+  return ec ? ec.value() : status;
 }
+
+// #include <cstdlib>
+// #include <fstream>
+// #include <iostream>
+// #include <reproc++/run.hpp>
+
+// int main() {
+//   // std::system("ls -l >test.txt"); // executes the UNIX command "ls -l
+//   // >test.txt" std::cout << std::ifstream("test.txt").rdbuf();
+//   reproc::run("ls -l >test.txt");
+//   std::cout << std::ifstream("test.txt").rdbuf();
+// }
+
+// // #include <iomanip>
+// // #include <iostream>
+// // #include <ranges>
+// // #include <string_view>
+
+// // int main() {
+// //   using std::operator""sv;
+// //   constexpr auto words{"Hello^_^C++^_^20^_^!"sv};
+// //   constexpr auto delim{"^_^"sv};
+
+// //   for (const auto word : std::views::split(words, delim))
+// //     // with string_view's C++23 range constructor:
+// //     std::cout << std::quoted(std::string_view(word)) << ' ';
+// //   std::cout << '\n';
+// // }
