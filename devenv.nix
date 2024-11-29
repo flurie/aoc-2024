352c46018e80f880f76d3267ca0e1602cd438ddb
flurie <flurie@gmail.com>
2024-11-29 14:45:03 -0500

    add back git hook checks

--- devenv.nix
+++ devenv.nix
@@ -1,63 +1,63 @@
-{ pkgs, ... }: {
-  packages = [ pkgs.bazel_7 pkgs.clang-tools pkgs.reproc ];
+{ pkgs, ... }:
+{
+  packages = [
+    # build
+    pkgs.bazel_7
 
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks = {
-  #   # C++
-  #   clang-format = {
-  #     enable = true;
-  #     types_or = [ "c++" "c" ];
-  #   };
-  #   clang-tidy = {
-  #     enable = false;
-  #     types_or = [ "c++" "c" ];
-  #     entry = "clang-tidy -p build --fix";
-  #   };
-  #   cppcheck = {
-  #     enable = true;
-  #     types = [ "c++" ];
-  #     entry = ''
-  #       cppcheck \
-  #           --check-level=exhaustive \
-  #           --enable=performance \
-  #           --enable=portability \
-  #           --enable=style \
-  #           --enable=warning \
-  #           --library=qt \
-  #           --error-exitcode=1 \
-  #           --inline-suppr \
-  #           --suppress=unusedStructMember \
-  #           --suppress=ctuOneDefinitionRuleViolation
-  #     '';
-  #   };
-  #   flawfinder = {
-  #     enable = true;
-  #     pass_filenames = false;
-  #     entry = "flawfinder --error-level=0 ./src";
-  #   };
+    # LSP
+    pkgs.clang-tools
 
-  #   # # CMake
-  #   # cmake-format = {
-  #   #   enable = true;
-  #   #   types = ["cmake"];
-  #   #   entry = "cmake-format -c .cmake.yaml -i";
-  #   # };
-  #   # cmake-lint = {
-  #   #   enable = true;
-  #   #   types = ["cmake"];
-  #   #   entry = "cmake-lint -c .cmake.yaml";
-  #   # };
+    # static analysis
+    pkgs.cppcheck
+    pkgs.flawfinder
 
-  #   # # Python
-  #   # ruff.enable = true;
+    # C++ libs
+    pkgs.reproc
+  ];
 
-  #   # nix
-  #   nixfmt.enable = true;
-  #   deadnix.enable = true;
-  #   flake-checker.enable = true;
-  #   nil.enable = true;
-  #   statix.enable = true;
-  # };
+  git-hooks.hooks = {
+    # C++
+    clang-format = {
+      enable = true;
+      types_or = [
+        "c++"
+        "c"
+      ];
+    };
+    clang-tidy = {
+      enable = false;
+      types_or = [
+        "c++"
+        "c"
+      ];
+      entry = "clang-tidy -p build --fix";
+    };
+    cppcheck = {
+      enable = true;
+      types = [ "c++" ];
+      entry = ''
+        cppcheck \
+            --check-level=exhaustive \
+            --enable=performance \
+            --enable=portability \
+            --enable=style \
+            --enable=warning \
+            --library=qt \
+            --error-exitcode=1 \
+            --inline-suppr \
+            --suppress=unusedStructMember \
+            --suppress=ctuOneDefinitionRuleViolation
+      '';
+    };
+    flawfinder = {
+      enable = true;
+      pass_filenames = false;
+      entry = "${pkgs.flawfinder}/bin/flawfinder --error-level=0 ./src";
+    };
+
+    # nix
+    nixfmt-rfc-style.enable = true;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
